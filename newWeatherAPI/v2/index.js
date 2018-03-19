2ae0e0dee96e93d4e3b639149f8302d9558c36a1
Leah <rhodycodes401@gmail.com>
2018-03-19 01:42:12 +0000

    fix toggle

--- newWeatherAPI/v2/index.js
+++ newWeatherAPI/v2/index.js
@@ -10,16 +10,10 @@ $(function(){
     var backgroundImg = ["image1", "image2",];
     var lat = 0;
     var lon = 0;
-    
-    function displayTemp (F,C){
-        var C = 0;
-        var F = 0;
-        if(C) return Math.round((F-32)*(5/9)) + '&deg; C';
-        console.log(Math.round(F) + '&deg; F');
-        console.log(C);
-        console.log(F);
-    }
-    
+    var F = false;
+    var C 
+
+
 //
     $("#locationHere").click(function(){
        
@@ -35,23 +29,65 @@ $(function(){
     });
     
     $("#weatherHere").click(function(){
-        console.log("did the functions start?");
-        $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat='+/*location.latitude*/+'&lon='+/*location.longitude*/+'&units=imperial&appid='+APIKEY).done(function(data){
-            console.log("did the data get logged?");
-            apiData = data;
-            console.log(data);
-            $('#yourDescription').html(data.weather[0].description);
-            $('#yourWind').html(data.wind.speed);
-            $('#yourTemp').html(data.main.temp + '\&deg\F');
+        
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var lat = position.coords.latitude;
+                var lon = position.coords.longitude;
+                console.log("Coordinates");
+            
+        
+                    console.log("did the functions start?");
+                    $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&units=imperial&appid='+APIKEY).done(function(data){
+                        console.log("did the data get logged?");
+                        apiData = data;
+                        console.log(data);
+                        $('#yourDescription').html(data.weather[0].description);
+                        $('#yourWind').html(data.wind.speed);
+                        $('#yourTemp').html(data.main.temp + '\&deg\F');
+                        
+                        
+                        //toggle function here:
+                        $("#degreesToggle").click(function(){
+                            var F = data.main.temp;
+                            console.log(F);
+                            var C = 0;
+                            console.log(C);
+                            
+                             $("#yourTemp").toggle(function() {
+                                 
+                                 $('#yourTemp').html(C+'\&deg\C');
+                             });
+                             console.log("toggle");
+                             
+                             
+                             
+                             
+                             
+                             
+                             
+                        });
+                    })
         });
-        });
+        }
     });
     
     
+});
 
 
-
-
+    
+   /* function displayTemp (F,C){
+        /*var C = 0;
+       /* var F = 0;
+       if(!F) {
+            console.log("error");
+        } else {return Math.round((F-32)*(5/9)) + '&deg; C';
+        console.log(Math.round(F) + '&deg; F');
+        console.log(C);
+        console.log(F);
+    }
+    }*/
 
 
 
