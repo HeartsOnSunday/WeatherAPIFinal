3c0a69ddc5322b87ea1f8f55ce5772c19b01761e
Leah <rhodycodes401@gmail.com>
2018-03-19 00:27:55 +0000

    new api

--- newWeatherAPI/v2/index.js
+++ newWeatherAPI/v2/index.js
@@ -2,28 +2,36 @@
 /* global $ */
 /* global APIKEY */
 /* global position */
+/* global location*/
 
 $(function(){
-    var C = false;
+   // var C = false;
     var apiData;
     var backgroundImg = ["image1", "image2",];
     
     function displayTemp (F,C){
+        var C = 0;
+        var F = 0;
         if(C) return Math.round((F-32)*(5/9)) + '&deg; C';
-        return Math.round(F) + '&deg; F';
+        console.log(Math.round(F) + '&deg; F');
         console.log(C);
         console.log(F);
-        console.log("WTF");
     }
     
-    $.getJSON('//freegeoip.net/json/').done(function(location){
+//
+    $("#locationHere").click(function(){
+        $.getJSON('//freegeoip.net/json/').done(function(location){
             console.log(location);
             $('#yourCity').html(location.city);
             $('#yourLatitude').html(location.latitude);
             $('#yourLongitude').html(location.longitude);
             console.log('the weather api call is about to happen');
-            
-        $.getJSON('//api.openweathermap.org/data/2.5/weather?lat='
+        });
+    });
+    
+    $("#weatherHere").click(function(){
+        console.log("did the functions start?");
+        $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat='
         +location.latitude+'&lon='
         +location.longitude+'&units=imperial&appid='+APIKEY, 
         function(data){
@@ -34,10 +42,10 @@ $(function(){
             $('#yourTemp').html(data.main.temp + '\&deg\F');
         });
         });
+    });
     
     
-    
-});
+
 
 
 
