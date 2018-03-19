75c95cbc4bbfb99ba845ab845f193d393ce7ff6d
Leah <rhodycodes401@gmail.com>
2018-03-19 00:45:37 +0000

    Lat Lon Variables

--- newWeatherAPI/v2/index.js
+++ newWeatherAPI/v2/index.js
@@ -6,8 +6,10 @@
 
 $(function(){
    // var C = false;
-    var apiData;
+    var apiData  = "";
     var backgroundImg = ["image1", "image2",];
+    var lat = 0;
+    var lon = 0;
     
     function displayTemp (F,C){
         var C = 0;
@@ -20,21 +22,22 @@ $(function(){
     
 //
     $("#locationHere").click(function(){
+       
         $.getJSON('//freegeoip.net/json/').done(function(location){
+            var lat = location.latitude;
+            var lon = location.longitude;
             console.log(location);
             $('#yourCity').html(location.city);
-            $('#yourLatitude').html(location.latitude);
-            $('#yourLongitude').html(location.longitude);
+            $('#yourLatitude').html(lat);
+            $('#yourLongitude').html(lon);
             console.log('the weather api call is about to happen');
         });
     });
     
     $("#weatherHere").click(function(){
         console.log("did the functions start?");
-        $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat='
-        +location.latitude+'&lon='
-        +location.longitude+'&units=imperial&appid='+APIKEY, 
-        function(data){
+        $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat='+/*location.latitude*/+'&lon='+/*location.longitude*/+'&units=imperial&appid='+APIKEY).done(function(data){
+            console.log("did the data get logged?");
             apiData = data;
             console.log(data);
             $('#yourDescription').html(data.weather[0].description);
